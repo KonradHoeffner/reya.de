e1cd97587b4444b0cad5858716755248b5fc9872
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-12-23 16:51:10 +0100

    Add multiple calendars.

--- js/calendar.js
+++ js/calendar.js
@@ -3,18 +3,22 @@ document.addEventListener('DOMContentLoaded',()=>
 	const calendarEl = document.getElementById('calendar');
 	const options =
 	{
+		height: 'auto',
 		header:
 		{
-        	left: 'prev,next today',
-	        center: 'title',
-    	    right: 'day,month,listYear'
-      	},
+			left: 'prev,next today',
+			center: 'title',
+			right: 'day,month,listWeek'
+		},
 		defaultView: 'listWeek',
 		locale: 'de',
 		googleCalendarApiKey: 'AIzaSyDE3COJLnCpJ2uqL9WlvU2bcX-oj3OgNaA',//TCG Warehouse, for testing. Integrate or show multiple calendars at once later.
-		events: '8m4pqn7179m8vof6qjvq8o3eag@group.calendar.google.com'
-		//googleCalendarId: 't2k7qiv3s455pcapd7g4otqji0@group.calendar.google.com'//reya.de
-	}
+		eventSources:
+		[
+			'8m4pqn7179m8vof6qjvq8o3eag@group.calendar.google.com',	// TCG Warehouse
+			't2k7qiv3s455pcapd7g4otqji0@group.calendar.google.com'	//	reya.de
+		]
+	};
 	const calendar = new FullCalendar.Calendar(calendarEl,options);
 	calendar.render();
 });

d2583e53da14da6ca5aa3f490fdcfc48bf7277e7
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-12-23 15:41:55 +0100

    Integrate Google Calendar. Use TCG Warehouse Calendar for testing.

--- js/calendar.js
+++ js/calendar.js
@@ -3,8 +3,17 @@ document.addEventListener('DOMContentLoaded',()=>
 	const calendarEl = document.getElementById('calendar');
 	const options =
 	{
+		header:
+		{
+        	left: 'prev,next today',
+	        center: 'title',
+    	    right: 'day,month,listYear'
+      	},
 		defaultView: 'listWeek',
-		locale: 'de'
+		locale: 'de',
+		googleCalendarApiKey: 'AIzaSyDE3COJLnCpJ2uqL9WlvU2bcX-oj3OgNaA',//TCG Warehouse, for testing. Integrate or show multiple calendars at once later.
+		events: '8m4pqn7179m8vof6qjvq8o3eag@group.calendar.google.com'
+		//googleCalendarId: 't2k7qiv3s455pcapd7g4otqji0@group.calendar.google.com'//reya.de
 	}
 	const calendar = new FullCalendar.Calendar(calendarEl,options);
 	calendar.render();
